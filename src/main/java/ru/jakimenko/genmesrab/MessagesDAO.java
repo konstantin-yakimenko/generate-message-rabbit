9316cc42532f3574d2f19fc9be0c8f09cca965c1
kyyakime <jakimenko.k@gmail.com>
2017-03-27 10:22:24 +0300

    commit

--- src/main/java/ru/jakimenko/genmesrab/MessagesDAO.java
+++ src/main/java/ru/jakimenko/genmesrab/MessagesDAO.java
@@ -1,6 +1,7 @@
 package ru.jakimenko.genmesrab;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -14,22 +15,14 @@ import java.util.List;
 @Component
 public class MessagesDAO {
 
-    private static final String SQL_SELECT =
-                    "select distinct value \n" +
-                    "from CUST.EntityPropertyValue \n" +
-                    "where entityPropertyKindId = 1 \n" +
-                    "and timestamp(now()) > DateFrom \n" +
-                    "and timestamp(now()) < DateTo \n" +
-                    "limit 300";
-    private final JdbcTemplate jdbcTemplate;
+    @Value("${select.query}")
+    private String sql_select;
 
     @Autowired
-    public MessagesDAO(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
+    private DataSource dataSource;
 
     public List<Long> getTerminalDevices() throws Exception {
-        return jdbcTemplate.query(SQL_SELECT, (ResultSet rs, int i) -> {
+        return new JdbcTemplate(dataSource).query(sql_select, (ResultSet rs, int i) -> {
             return rs.getLong(1);
         });
     }
